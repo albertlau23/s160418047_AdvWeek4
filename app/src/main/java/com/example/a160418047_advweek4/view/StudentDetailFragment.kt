fdd9560acad3b18772fc18cb80e23696be5c58ae
ASUS <hermanlau89@gmail.com>
2021-09-06 19:09:20 +0800

    doone

--- app/src/main/java/com/example/a160418047_advweek4/view/StudentDetailFragment.kt
+++ app/src/main/java/com/example/a160418047_advweek4/view/StudentDetailFragment.kt
@@ -5,7 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.a160418047_advweek4.R
+import com.example.a160418047_advweek4.viewmodel.DetailViewModel
+import kotlinx.android.synthetic.main.fragment_student_detail.view.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -19,15 +23,19 @@ private const val ARG_PARAM2 = "param2"
  */
 class StudentDetailFragment : Fragment() {
     // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+    private lateinit var viewModel:DetailViewModel
+    private val studentListAdapter = StudentListAdapter(arrayListOf())
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailViewModel::class.java)
+        viewModel.fetch()
+        viewModel.studentLD.observe(viewLifecycleOwner, Observer {
+            view.txtName.setText(it.name)
+            view.txtID.setText(it.id)
+            view.txtBod.setText(it.bod)
+            view.txtPhone.setText(it.phone)
+        })
     }
 
     override fun onCreateView(
@@ -38,23 +46,5 @@ class StudentDetailFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_student_detail, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment StudentDetailFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            StudentDetailFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
-    }
+
 }
