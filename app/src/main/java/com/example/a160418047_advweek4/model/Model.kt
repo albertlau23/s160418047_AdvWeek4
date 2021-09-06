7c63e0e93693402429f0e3bab9914324156250ad
ASUS <hermanlau89@gmail.com>
2021-09-06 18:43:00 +0800

    init

---
+++ app/src/main/java/com/example/a160418047_advweek4/model/Model.kt
@@ -0,0 +1,9 @@
+package com.example.a160418047_advweek4.model
+
+data class Student(
+    val id:String?,
+    val name:String?,
+    val bod:String?,
+    val phone:String?,
+    val photourl:String?
+        )
